8077b5655c32adcad2dbd155f647679675672dd0
liaohao <whitealbum17@outlook.com>
2024-05-06 23:48:18 +0800

    菜品起售停售功能

--- sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
+++ sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
@@ -1,7 +1,12 @@
 package com.sky.mapper;
 
+import com.sky.annotation.AutoFill;
+import com.sky.entity.Setmeal;
+import com.sky.enumeration.OperationType;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 @Mapper
 public interface SetmealMapper {
@@ -14,4 +19,14 @@ public interface SetmealMapper {
     @Select("select count(id) from setmeal where category_id = #{categoryId}")
     Integer countByCategoryId(Long id);
 
+    /**
+     * 根据id修改套餐
+     *
+     * @param setmeal
+     */
+    @AutoFill(OperationType.UPDATE)
+    void update(Setmeal setmeal);
+
+    @Select("select setmeal_id from setmeal_dish where dish_id in (#{dishIds})")
+    List<Long> getSetmealIdsByDishIds(List<Long> dishIds);
 }
--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -40,4 +40,12 @@ public interface DishService {
      * @param dishDTO
      */
     void updateWithFlavor(DishDTO dishDTO);
+
+    /**
+     * 菜品起售停售
+     * @param status
+     * @param id
+     */
+    void startOrStop(Integer status, Long id);
+
 }
